4f08b0d50247ff8edccb2080c2be5cdf51dc3856
Vikram Praksh <vikram@nile-global.com>
2022-01-06 21:13:01 +0530

    spring boot app

---
+++ spring_boot_annotations/spring_boot_annotations/src/test/java/com/annotations/spring_boot_annotations/SpringBootAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annotations.spring_boot_annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
